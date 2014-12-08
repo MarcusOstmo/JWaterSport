b9f69c1b66fd60a70adfb357341042b024a38eb2
dethret <andreaspallin@live.se>
2014-12-08 10:49:54 +0100

    Added controler

---
+++ JWS/Controler/JavaWaterSportList.cs
@@ -0,0 +1,52 @@
+﻿using JWS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWS.Controler
+{
+    class JavaWaterSportList
+    {
+        private List<Kund> myKund;
+
+        public KundList()
+        {
+            myKund = new List<Kund>();
+        }
+
+        public void Add(Kund item)
+        {
+            myKund.Add(item);
+        }
+
+        public void Remove(Kund item)
+        {
+            myKund.Remove(item);
+        }
+
+        public Kund Get(int index)
+        {
+            return myKund.ElementAt(index);
+        }
+
+        public int Count()
+        {
+            return myKund.Count();
+        }
+
+
+        public Kund FindKund(string kund)
+        {
+            for (int index = 0; index < myKund.Count; index++)
+            {
+                if (myKund[index].getKund() == kund)
+                {
+                    return myKund[index];
+                }
+            }
+            return null;
+        }
+    }
+}
