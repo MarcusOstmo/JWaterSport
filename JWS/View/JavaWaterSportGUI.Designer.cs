aecbf0e6458389c7c2d0bbbe2dcff2ea0559f62a
KevinLundberg <kevinlundberg96@gmail.com>
2014-12-08 10:44:40 +0100

    View

--- JWS/View/JavaWaterSportGUI.Designer.cs
+++ JWS/View/JavaWaterSportGUI.Designer.cs
@@ -28,19 +28,300 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblFörnamn = new System.Windows.Forms.Label();
+            this.lblEfternamn = new System.Windows.Forms.Label();
+            this.lblAdress = new System.Windows.Forms.Label();
+            this.lblTelefon = new System.Windows.Forms.Label();
+            this.tbxFörnamn = new System.Windows.Forms.TextBox();
+            this.tbxEfternamn = new System.Windows.Forms.TextBox();
+            this.tbxAdress = new System.Windows.Forms.TextBox();
+            this.tbxTelefon = new System.Windows.Forms.TextBox();
+            this.rbnStortPaket = new System.Windows.Forms.RadioButton();
+            this.rbnEndast = new System.Windows.Forms.RadioButton();
+            this.btnRegistreraKund = new System.Windows.Forms.Button();
+            this.btnÄndraKunduppgifter = new System.Windows.Forms.Button();
+            this.cbxTidStortPaket = new System.Windows.Forms.ComboBox();
+            this.cbxTidEndast = new System.Windows.Forms.ComboBox();
+            this.lblRegistreraKund = new System.Windows.Forms.Label();
+            this.lblBokaDykarkurs = new System.Windows.Forms.Label();
+            this.btnBokaDykarkurs = new System.Windows.Forms.Button();
+            this.btnÄndraKursuppgifter = new System.Windows.Forms.Button();
+            this.lblPrisPerTimme = new System.Windows.Forms.Label();
+            this.cbxDykinstruktör = new System.Windows.Forms.ComboBox();
+            this.cbxTidDykarkurs = new System.Windows.Forms.ComboBox();
+            this.lbxAktuellKund = new System.Windows.Forms.ListBox();
+            this.btnTaBortKund = new System.Windows.Forms.Button();
+            this.btnSkrivUtKund = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblFörnamn
+            // 
+            this.lblFörnamn.AutoSize = true;
+            this.lblFörnamn.Location = new System.Drawing.Point(22, 47);
+            this.lblFörnamn.Name = "lblFörnamn";
+            this.lblFörnamn.Size = new System.Drawing.Size(51, 13);
+            this.lblFörnamn.TabIndex = 0;
+            this.lblFörnamn.Text = "Förnamn:";
+            // 
+            // lblEfternamn
+            // 
+            this.lblEfternamn.AutoSize = true;
+            this.lblEfternamn.Location = new System.Drawing.Point(23, 87);
+            this.lblEfternamn.Name = "lblEfternamn";
+            this.lblEfternamn.Size = new System.Drawing.Size(58, 13);
+            this.lblEfternamn.TabIndex = 1;
+            this.lblEfternamn.Text = "Efternamn:";
+            // 
+            // lblAdress
+            // 
+            this.lblAdress.AutoSize = true;
+            this.lblAdress.Location = new System.Drawing.Point(23, 131);
+            this.lblAdress.Name = "lblAdress";
+            this.lblAdress.Size = new System.Drawing.Size(42, 13);
+            this.lblAdress.TabIndex = 2;
+            this.lblAdress.Text = "Adress:";
+            // 
+            // lblTelefon
+            // 
+            this.lblTelefon.AutoSize = true;
+            this.lblTelefon.Location = new System.Drawing.Point(23, 176);
+            this.lblTelefon.Name = "lblTelefon";
+            this.lblTelefon.Size = new System.Drawing.Size(46, 13);
+            this.lblTelefon.TabIndex = 3;
+            this.lblTelefon.Text = "Telefon:";
+            // 
+            // tbxFörnamn
+            // 
+            this.tbxFörnamn.Location = new System.Drawing.Point(87, 44);
+            this.tbxFörnamn.Name = "tbxFörnamn";
+            this.tbxFörnamn.Size = new System.Drawing.Size(100, 20);
+            this.tbxFörnamn.TabIndex = 4;
+            // 
+            // tbxEfternamn
+            // 
+            this.tbxEfternamn.Location = new System.Drawing.Point(87, 83);
+            this.tbxEfternamn.Name = "tbxEfternamn";
+            this.tbxEfternamn.Size = new System.Drawing.Size(100, 20);
+            this.tbxEfternamn.TabIndex = 5;
+            // 
+            // tbxAdress
+            // 
+            this.tbxAdress.Location = new System.Drawing.Point(87, 128);
+            this.tbxAdress.Name = "tbxAdress";
+            this.tbxAdress.Size = new System.Drawing.Size(100, 20);
+            this.tbxAdress.TabIndex = 6;
+            // 
+            // tbxTelefon
+            // 
+            this.tbxTelefon.Location = new System.Drawing.Point(87, 176);
+            this.tbxTelefon.Name = "tbxTelefon";
+            this.tbxTelefon.Size = new System.Drawing.Size(100, 20);
+            this.tbxTelefon.TabIndex = 7;
+            // 
+            // rbnStortPaket
+            // 
+            this.rbnStortPaket.AutoSize = true;
+            this.rbnStortPaket.Location = new System.Drawing.Point(23, 229);
+            this.rbnStortPaket.Name = "rbnStortPaket";
+            this.rbnStortPaket.Size = new System.Drawing.Size(78, 17);
+            this.rbnStortPaket.TabIndex = 8;
+            this.rbnStortPaket.TabStop = true;
+            this.rbnStortPaket.Text = "Stort Paket";
+            this.rbnStortPaket.UseVisualStyleBackColor = true;
+            // 
+            // rbnEndast
+            // 
+            this.rbnEndast.AutoSize = true;
+            this.rbnEndast.Location = new System.Drawing.Point(23, 275);
+            this.rbnEndast.Name = "rbnEndast";
+            this.rbnEndast.Size = new System.Drawing.Size(58, 17);
+            this.rbnEndast.TabIndex = 9;
+            this.rbnEndast.TabStop = true;
+            this.rbnEndast.Text = "Endast";
+            this.rbnEndast.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistreraKund
+            // 
+            this.btnRegistreraKund.Location = new System.Drawing.Point(23, 411);
+            this.btnRegistreraKund.Name = "btnRegistreraKund";
+            this.btnRegistreraKund.Size = new System.Drawing.Size(97, 23);
+            this.btnRegistreraKund.TabIndex = 10;
+            this.btnRegistreraKund.Text = "Registrera Kund";
+            this.btnRegistreraKund.UseVisualStyleBackColor = true;
+            // 
+            // btnÄndraKunduppgifter
+            // 
+            this.btnÄndraKunduppgifter.Location = new System.Drawing.Point(23, 473);
+            this.btnÄndraKunduppgifter.Name = "btnÄndraKunduppgifter";
+            this.btnÄndraKunduppgifter.Size = new System.Drawing.Size(119, 23);
+            this.btnÄndraKunduppgifter.TabIndex = 11;
+            this.btnÄndraKunduppgifter.Text = "Ändra Kunduppgifter";
+            this.btnÄndraKunduppgifter.UseVisualStyleBackColor = true;
+            // 
+            // cbxTidStortPaket
+            // 
+            this.cbxTidStortPaket.FormattingEnabled = true;
+            this.cbxTidStortPaket.Location = new System.Drawing.Point(115, 229);
+            this.cbxTidStortPaket.Name = "cbxTidStortPaket";
+            this.cbxTidStortPaket.Size = new System.Drawing.Size(72, 21);
+            this.cbxTidStortPaket.TabIndex = 12;
+            // 
+            // cbxTidEndast
+            // 
+            this.cbxTidEndast.FormattingEnabled = true;
+            this.cbxTidEndast.Location = new System.Drawing.Point(115, 275);
+            this.cbxTidEndast.Name = "cbxTidEndast";
+            this.cbxTidEndast.Size = new System.Drawing.Size(72, 21);
+            this.cbxTidEndast.TabIndex = 13;
+            // 
+            // lblRegistreraKund
+            // 
+            this.lblRegistreraKund.AutoSize = true;
+            this.lblRegistreraKund.Location = new System.Drawing.Point(52, 9);
+            this.lblRegistreraKund.Name = "lblRegistreraKund";
+            this.lblRegistreraKund.Size = new System.Drawing.Size(80, 13);
+            this.lblRegistreraKund.TabIndex = 14;
+            this.lblRegistreraKund.Text = "Registera Kund";
+            // 
+            // lblBokaDykarkurs
+            // 
+            this.lblBokaDykarkurs.AutoSize = true;
+            this.lblBokaDykarkurs.Location = new System.Drawing.Point(338, 12);
+            this.lblBokaDykarkurs.Name = "lblBokaDykarkurs";
+            this.lblBokaDykarkurs.Size = new System.Drawing.Size(83, 13);
+            this.lblBokaDykarkurs.TabIndex = 15;
+            this.lblBokaDykarkurs.Text = "Boka Dykarkurs";
+            // 
+            // btnBokaDykarkurs
+            // 
+            this.btnBokaDykarkurs.Location = new System.Drawing.Point(247, 125);
+            this.btnBokaDykarkurs.Name = "btnBokaDykarkurs";
+            this.btnBokaDykarkurs.Size = new System.Drawing.Size(92, 23);
+            this.btnBokaDykarkurs.TabIndex = 16;
+            this.btnBokaDykarkurs.Text = "Boka Dykarkurs";
+            this.btnBokaDykarkurs.UseVisualStyleBackColor = true;
+            // 
+            // btnÄndraKursuppgifter
+            // 
+            this.btnÄndraKursuppgifter.Location = new System.Drawing.Point(393, 125);
+            this.btnÄndraKursuppgifter.Name = "btnÄndraKursuppgifter";
+            this.btnÄndraKursuppgifter.Size = new System.Drawing.Size(109, 23);
+            this.btnÄndraKursuppgifter.TabIndex = 17;
+            this.btnÄndraKursuppgifter.Text = "Ändra Kursuppgifter";
+            this.btnÄndraKursuppgifter.UseVisualStyleBackColor = true;
+            // 
+            // lblPrisPerTimme
+            // 
+            this.lblPrisPerTimme.AutoSize = true;
+            this.lblPrisPerTimme.Location = new System.Drawing.Point(460, 47);
+            this.lblPrisPerTimme.Name = "lblPrisPerTimme";
+            this.lblPrisPerTimme.Size = new System.Drawing.Size(42, 13);
+            this.lblPrisPerTimme.TabIndex = 18;
+            this.lblPrisPerTimme.Text = "200:-/h";
+            // 
+            // cbxDykinstruktör
+            // 
+            this.cbxDykinstruktör.FormattingEnabled = true;
+            this.cbxDykinstruktör.Location = new System.Drawing.Point(247, 44);
+            this.cbxDykinstruktör.Name = "cbxDykinstruktör";
+            this.cbxDykinstruktör.Size = new System.Drawing.Size(121, 21);
+            this.cbxDykinstruktör.TabIndex = 19;
+            // 
+            // cbxTidDykarkurs
+            // 
+            this.cbxTidDykarkurs.FormattingEnabled = true;
+            this.cbxTidDykarkurs.Location = new System.Drawing.Point(383, 44);
+            this.cbxTidDykarkurs.Name = "cbxTidDykarkurs";
+            this.cbxTidDykarkurs.Size = new System.Drawing.Size(71, 21);
+            this.cbxTidDykarkurs.TabIndex = 20;
+            // 
+            // lbxAktuellKund
+            // 
+            this.lbxAktuellKund.FormattingEnabled = true;
+            this.lbxAktuellKund.Location = new System.Drawing.Point(533, 23);
+            this.lbxAktuellKund.Name = "lbxAktuellKund";
+            this.lbxAktuellKund.Size = new System.Drawing.Size(652, 433);
+            this.lbxAktuellKund.TabIndex = 21;
+            // 
+            // btnTaBortKund
+            // 
+            this.btnTaBortKund.Location = new System.Drawing.Point(533, 473);
+            this.btnTaBortKund.Name = "btnTaBortKund";
+            this.btnTaBortKund.Size = new System.Drawing.Size(80, 23);
+            this.btnTaBortKund.TabIndex = 22;
+            this.btnTaBortKund.Text = "Ta Bort Kund";
+            this.btnTaBortKund.UseVisualStyleBackColor = true;
+            // 
+            // btnSkrivUtKund
+            // 
+            this.btnSkrivUtKund.Location = new System.Drawing.Point(1088, 473);
+            this.btnSkrivUtKund.Name = "btnSkrivUtKund";
+            this.btnSkrivUtKund.Size = new System.Drawing.Size(97, 23);
+            this.btnSkrivUtKund.TabIndex = 23;
+            this.btnSkrivUtKund.Text = "Skriv Ut Kvitto";
+            this.btnSkrivUtKund.UseVisualStyleBackColor = true;
             // 
             // JavaWaterSportGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1153, 649);
+            this.ClientSize = new System.Drawing.Size(1212, 516);
+            this.Controls.Add(this.btnSkrivUtKund);
+            this.Controls.Add(this.btnTaBortKund);
+            this.Controls.Add(this.lbxAktuellKund);
+            this.Controls.Add(this.cbxTidDykarkurs);
+            this.Controls.Add(this.cbxDykinstruktör);
+            this.Controls.Add(this.lblPrisPerTimme);
+            this.Controls.Add(this.btnÄndraKursuppgifter);
+            this.Controls.Add(this.btnBokaDykarkurs);
+            this.Controls.Add(this.lblBokaDykarkurs);
+            this.Controls.Add(this.lblRegistreraKund);
+            this.Controls.Add(this.cbxTidEndast);
+            this.Controls.Add(this.cbxTidStortPaket);
+            this.Controls.Add(this.btnÄndraKunduppgifter);
+            this.Controls.Add(this.btnRegistreraKund);
+            this.Controls.Add(this.rbnEndast);
+            this.Controls.Add(this.rbnStortPaket);
+            this.Controls.Add(this.tbxTelefon);
+            this.Controls.Add(this.tbxAdress);
+            this.Controls.Add(this.tbxEfternamn);
+            this.Controls.Add(this.tbxFörnamn);
+            this.Controls.Add(this.lblTelefon);
+            this.Controls.Add(this.lblAdress);
+            this.Controls.Add(this.lblEfternamn);
+            this.Controls.Add(this.lblFörnamn);
             this.Name = "JavaWaterSportGUI";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblFörnamn;
+        private System.Windows.Forms.Label lblEfternamn;
+        private System.Windows.Forms.Label lblAdress;
+        private System.Windows.Forms.Label lblTelefon;
+        private System.Windows.Forms.TextBox tbxFörnamn;
+        private System.Windows.Forms.TextBox tbxEfternamn;
+        private System.Windows.Forms.TextBox tbxAdress;
+        private System.Windows.Forms.TextBox tbxTelefon;
+        private System.Windows.Forms.RadioButton rbnStortPaket;
+        private System.Windows.Forms.RadioButton rbnEndast;
+        private System.Windows.Forms.Button btnRegistreraKund;
+        private System.Windows.Forms.Button btnÄndraKunduppgifter;
+        private System.Windows.Forms.ComboBox cbxTidStortPaket;
+        private System.Windows.Forms.ComboBox cbxTidEndast;
+        private System.Windows.Forms.Label lblRegistreraKund;
+        private System.Windows.Forms.Label lblBokaDykarkurs;
+        private System.Windows.Forms.Button btnBokaDykarkurs;
+        private System.Windows.Forms.Button btnÄndraKursuppgifter;
+        private System.Windows.Forms.Label lblPrisPerTimme;
+        private System.Windows.Forms.ComboBox cbxDykinstruktör;
+        private System.Windows.Forms.ComboBox cbxTidDykarkurs;
+        private System.Windows.Forms.ListBox lbxAktuellKund;
+        private System.Windows.Forms.Button btnTaBortKund;
+        private System.Windows.Forms.Button btnSkrivUtKund;
     }
 }
